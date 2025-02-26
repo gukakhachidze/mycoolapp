67b0b26e5f96061c69ed000e32426e50d378e889
Guka Khachidze <guka.khachidze@gmail.com>
2025-02-26 21:22:20 +0400

    Create Maven Project For Practice

---
+++ src/test/java/ge/gukas/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.gukas.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
